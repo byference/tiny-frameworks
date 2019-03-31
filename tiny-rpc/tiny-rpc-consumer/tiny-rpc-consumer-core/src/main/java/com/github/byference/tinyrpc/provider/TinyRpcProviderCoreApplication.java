b18de10830b02999adf93c8d9a82bd91ac138fb5
byference <1170126481@qq.com>
2019-03-31 15:07:55 +0800

    :sparkles: tiny-rpc init

---
+++ tiny-rpc/tiny-rpc-consumer/tiny-rpc-consumer-core/src/main/java/com/github/byference/tinyrpc/provider/TinyRpcProviderCoreApplication.java
@@ -0,0 +1,17 @@
+package com.github.byference.tinyrpc.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author byference
+ * @since 2019/03/31
+ */
+@SpringBootApplication
+public class TinyRpcProviderCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyRpcProviderCoreApplication.class, args);
+    }
+
+}
