332c2c828bf6b73e1f3a251a402cf559c31345f5
byference <1170126481@qq.com>
2019-04-05 20:41:08 +0800

    in preparation for tiny-rpc

---
+++ tiny-rpc/tiny-rpc-core/src/main/java/com/github/byference/tinyrpc/common/TinyRpcConst.java
@@ -0,0 +1,15 @@
+package com.github.byference.tinyrpc.common;
+
+/**
+ * TinyRpcConst
+ *
+ * @author byference
+ * @since 2019/04/01
+ */
+public class TinyRpcConst {
+
+    /**
+     * 提供服务的地址信息
+     */
+    public static final String SERVER_ADDRESS = "127.0.0.1:9090";
+}
---
+++ tiny-rpc/tiny-rpc-core/src/main/java/com/github/byference/tinyrpc/util/StringUtil.java
@@ -0,0 +1,25 @@
+package com.github.byference.tinyrpc.util;
+
+/**
+ * StringUtil
+ *
+ * @author byference
+ * @since 2019/04/05
+ */
+public class StringUtil {
+
+
+    /**
+     * 将字符串的首字母小写
+     * @param str 待处理字符串
+     * @return 首字母小写后的字符串
+     */
+    public static String lowerFirst(String str) {
+        char[] chars = str.toCharArray();
+        if (Character.isUpperCase(chars[0])) {
+            chars[0] += 32;
+        }
+        return String.valueOf(chars);
+    }
+
+}
