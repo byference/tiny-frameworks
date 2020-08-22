63934c36f3693205923a0b4682a972da3ad0ac61
byference <byference@foxmail.com>
2020-08-22 21:30:04 +0800

    feat(tiny-mapper): init

---
+++ tiny-mapper/src/main/java/io/github/byference/mapper/TinyMapperApplication.java
@@ -0,0 +1,19 @@
+package io.github.byference.mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TinyMapperApplication
+ *
+ * @author byference
+ * @since 2020-08-22
+ */
+@SpringBootApplication
+public class TinyMapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyMapperApplication.class, args);
+    }
+
+}
